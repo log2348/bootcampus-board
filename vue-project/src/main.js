cd1bbd5b17b455df186103c9ab2b4868a4e2498c
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-10-24 08:45:58 +0900

    프로젝트 기본 설정, 컴포넌트 생성

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import axios from "axios";
+import router from "./router/index.js";
+import store from "./store/index.js";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.prototype.$axios = axios;
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  axios,
+  store,
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
